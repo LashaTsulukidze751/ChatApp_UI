6facb73872d3b5d51c2d053d929b392319341c55
Lasha <babaxatsulukidze@gmail.com>
2024-02-29 15:14:00 +0400

    "feat: search usernames"

--- app/components/Sidenav.tsx
+++ app/components/Sidenav.tsx
@@ -14,6 +14,7 @@ export default function Sizenav() {
   const [users, setUsers] = useState<Users[]>([
     { username: "", usersurname: "", profileimage: "" },
   ]);
+  const [searchUser, setSearchUser] = useState("");
 
   useEffect(() => {
     let ignore = false;
@@ -31,32 +32,43 @@ export default function Sizenav() {
 
   return (
     <div className="w-1/6 border-r border-gray md:w-2/6 lg:w-1/4 xl:w-1/6">
-      <p>search</p>
+      <input
+        className="m-2 h-7 w-[94%] pl-1 text-black outline-amber-400 placeholder:text-black"
+        placeholder="Search users"
+        onChange={(e) => {
+          setSearchUser(e.target.value);
+        }}
+      />
       <div className="h-full overflow-y-scroll">
-        {users.map((user) => (
-          <Link
-            key={user.username}
-            href={{
-              pathname: "/main/chat/chatroom",
-              query: { name: user.username },
-            }}
-          >
-            <div
-              className="m-2 flex flex-col items-center md:flex-row-reverse md:justify-end"
-              onClick={() => handleUsersGet()}
-            >
-              <div className="flex md:ml-4">
-                <p>{user.username}</p>
-                <p className="ml-1 hidden md:inline">{user.usersurname}</p>
-              </div>
-              <img
-                className="size-14 overflow-hidden rounded-full"
-                src={user.profileimage}
-                alt="Picture of the author"
-              />
-            </div>
-          </Link>
-        ))}
+        {users.map((user) => {
+          let fullname = user.username + user.usersurname;
+          return (
+            fullname.includes(searchUser) && (
+              <Link
+                key={user.username}
+                href={{
+                  pathname: "/main/chat/chatroom",
+                  query: { name: user.username },
+                }}
+              >
+                <div
+                  className="m-2 flex flex-col items-center hover:text-dark-gold md:flex-row-reverse md:justify-end"
+                  onClick={() => handleUsersGet()}
+                >
+                  <div className="flex md:ml-4">
+                    <p>{user.username}</p>
+                    <p className="ml-1 hidden md:inline">{user.usersurname}</p>
+                  </div>
+                  <img
+                    className="size-14 overflow-hidden rounded-full"
+                    src={user.profileimage}
+                    alt="Picture of the author"
+                  />
+                </div>
+              </Link>
+            )
+          );
+        })}
       </div>
     </div>
   );
